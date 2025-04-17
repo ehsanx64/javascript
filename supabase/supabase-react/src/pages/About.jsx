2c352df16aba414463a4e2511588278bbc5ceb97
Ehsan Mohammadi <ehsanx64@gmail.com>
2025-04-17 21:07:58 +0330

    supabase: add material-ui to supabase-react

--- supabase/supabase-react/src/pages/About.jsx
+++ supabase/supabase-react/src/pages/About.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
-
+import Button from '@mui/material/Button';
 
 function About() {
     return (
         <>
             <h3>About</h3>
+        
+            <Button variant="contained">Hello world</Button>
         </>
     )
 }
